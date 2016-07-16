fb3b90537f067b089947421aa16462710907a0ca
NedalKouissi <wil2173256@maricopa.edu>
2016-07-16 03:09:30 +0100

    Add ES6 constructor pattern

--- constructor.js
+++ constructor.js
@@ -1,3 +1,22 @@
+// ES6
+class car {
+  constructor (opt) {
+    Object.assign(this, opt)
+  }
+
+  toString () {
+    return `${this.model}, ${this.year}`
+  }
+}
+
+// USAGE:
+const ferrari = new Car({
+  model: 'ferrari',
+  year: 2015
+})
+console.log(ferrari.toString())
+
+// ES5
 function Car (model, year) {
   this.model = model
   this.year = year
